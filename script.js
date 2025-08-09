5865ac83d6c49bbf4b553630153275eeba363e7c
Pietra <pietracosta.dev@gmail.com>
2025-08-09 11:29:18 -0300

    script accordion

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li")
-const tabContent = document.querySelectorAll(".js-tabcontent section")
 
-tabContent[0].classList.add("ativo")
+function addTab() {
+    const tabMenu = document.querySelectorAll(".js-tabmenu li")
+    const tabContent = document.querySelectorAll(".js-tabcontent section")
 
-function activeTab(index){
-    tabContent.forEach((item) =>{
-        item.classList.remove("ativo")
+    tabContent[0].classList.add("ativo")
+
+    function activeTab(index) {
+        tabContent.forEach((item) => {
+            item.classList.remove("ativo")
+        })
+
+        tabContent[index].classList.add("ativo")
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener("click", function () {
+            activeTab(index)
+        })
     })
-
-    tabContent[index].classList.add("ativo")
 }
 
-tabMenu.forEach((itemMenu, index) =>{
-    itemMenu.addEventListener("click", function(){
-        activeTab(index)
+addTab()
+
+function accordion() {
+    const faqAccordion = document.querySelectorAll(".js-accordion dt")
+    const faqAccordionDd = document.querySelectorAll(".js-accordion dd")
+
+    faqAccordion[0].classList.add("ativo")
+    faqAccordionDd[0].classList.add("ativo")
+
+    function handleActiveClass() {
+        this.classList.toggle("ativo")
+        this.nextElementSibling.classList.toggle("ativo")
+    }
+
+    faqAccordion.forEach((item) => {
+        item.addEventListener("click", handleActiveClass)
     })
-})
+}
+
+
+accordion()
