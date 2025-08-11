84d1306663347a61e6ecefc400a60007379779f2
Pietra <pietracosta.dev@gmail.com>
2025-08-11 13:14:35 -0300

    scroll suave link internos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+//anbimacao descricao e imagens
+
 function addTab() {
     const tabMenu = document.querySelectorAll(".js-tabmenu li")
     const tabContent = document.querySelectorAll(".js-tabcontent section")
@@ -22,16 +24,19 @@ function addTab() {
 
 addTab()
 
+//accordion para faq
+
 function accordion() {
+    const ativo = 'ativo';
     const faqAccordion = document.querySelectorAll(".js-accordion dt")
     const faqAccordionDd = document.querySelectorAll(".js-accordion dd")
 
-    faqAccordion[0].classList.add("ativo")
-    faqAccordionDd[0].classList.add("ativo")
+    faqAccordion[0].classList.add(ativo)
+    faqAccordionDd[0].classList.add(ativo)
 
     function handleActiveClass() {
-        this.classList.toggle("ativo")
-        this.nextElementSibling.classList.toggle("ativo")
+        this.classList.toggle(ativo)
+        this.nextElementSibling.classList.toggle(ativo)
     }
 
     faqAccordion.forEach((item) => {
@@ -41,3 +46,29 @@ function accordion() {
 
 
 accordion()
+
+//scroll suave link interno
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function handleScroll(event){
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+
+    //FORMA ALTERNATIVA
+    // const topo = section.offsetTop
+    // window.scrollTo({
+    //     top: topo,
+    //     behavior: 'smooth'
+    // })
+
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+}
+
+linksInternos.forEach((link) =>{
+    link.addEventListener("click", handleScroll)
+})
